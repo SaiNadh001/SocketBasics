83dc7a4c6c6fe2041edaddea60cb8b6cf2aa84f6
SaiNadh <saibaba.nadh@dreamorbit.com>
2015-09-22 13:41:17 +0530

    Simple chat app using chat

---
+++ server.js
@@ -0,0 +1,22 @@
+
+var app = require('express')();
+var port = process.env.PORT || 4000;
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection',function(socket){
+	socket.broadcast.emit('greet', "Hello everyone..!")
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+server.listen(port, function(){
+	console.log("running on : "+port)
+});
